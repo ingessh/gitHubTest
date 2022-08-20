c2369b22e88dc7772d925eb779108efe2713aef6
OswaldoJaime <oswaldojaimesolis@gmail.com>
2022-08-20 12:33:28 -0500

    cambio local

--- src/githubtest/GitHubTest.java
+++ src/githubtest/GitHubTest.java
@@ -28,7 +28,7 @@ public class GitHubTest extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
-                System.out.println("llamada al reporter");
+                System.out.println("llamada al reporter localmente");
                 
             }
         });
