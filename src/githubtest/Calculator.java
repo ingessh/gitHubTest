8586edc9ab7ac856e87b3a4572f56ac57f9332bf
OswaldoJaime <oswaldojaimesolis@gmail.com>
2022-08-20 10:56:04 -0500

    adding calculator add

---
+++ src/githubtest/Calculator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtest;
+
+/**
+ *
+ * @author Oswaldo
+ */
+public class Calculator {
+
+    public void calculatorAddDo(Double firtFactor, Double secondFactor) {
+        System.out.println(firtFactor + secondFactor);
+    }
+}
